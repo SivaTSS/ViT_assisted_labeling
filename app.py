00e783219663707fcba93bde2b33d628d8469521
SivaTSS <tsssiva1999@gmail.com>
2023-08-29 23:44:29 -0400

    formatted lint

--- app.py
+++ app.py
@@ -14,23 +14,27 @@ import cv2
 import supervision as sv
 import os
 
+# loading models
 grounding_dino_model = grounding_dino.load_model()
 sam_model = sam.load_model()
 
+
 class ImageAnnotatorApp:
     def __init__(self, root):
-        c1="#FAF1E4"
-        c2="#CEDEBD"
-        c3="#9EB384"
-        c4="#435334"
-        self.CLASSES = ['bottle', 'dog', 'person', 'nose', 'chair', 'car', 'ear']
+        c1 = "#FAF1E4"
+        c2 = "#CEDEBD"
+        c3 = "#9EB384"
+        c4 = "#435334"
+        # classes can be changed to focus on diffent objects
+        self.CLASSES = ["bottle", "dog", "person", "nose", "chair", "car", "ear"]
+
         self.root = root
         self.root.title("Image Annotator")
-        self.root.state('zoomed')
+        self.root.state("zoomed")
         self.root.configure(bg=c1)
 
-        self.screen_width= self.root.winfo_screenwidth() 
-        self.screen_height= self.root.winfo_screenheight() 
+        self.screen_width = self.root.winfo_screenwidth()
+        self.screen_height = self.root.winfo_screenheight()
         self.padx = self.screen_width // 50
         self.pady = self.screen_height // 50
         self.text_size = int(self.screen_height * 0.015)
@@ -43,7 +47,6 @@ class ImageAnnotatorApp:
         self.image_frame.grid_rowconfigure(0, weight=1)
         self.image_frame.grid_columnconfigure(0, weight=1)
         self.image_frame.grid_columnconfigure(1, weight=1)
-                
 
         self.loaded_image_frame = tk.Frame(self.image_frame, bd=1, bg=c2, highlightbackground=c4, relief=tk.SOLID)
         self.loaded_image_frame.grid(row=0, column=0, padx=self.padx, pady=self.pady, sticky="nsew")
@@ -55,12 +58,33 @@ class ImageAnnotatorApp:
         self.annotated_image_label = tk.Label(self.annotated_image_frame, bg=c2)
         self.annotated_image_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        self.load_button = tk.Button(self.root, text="Load Image", command=self.load_image, width=self.button_width,
-                                      height=self.button_height, bg=c3, font=self.button_font )
-        self.annotate_button = tk.Button(self.root, text="Annotate", command=self.annotate_image, width=self.button_width,
-                                          height=self.button_height, bg=c3, font=self.button_font )
-        self.quit_button = tk.Button(self.root, text="Quit", command=self.root.quit, width=self.button_width,
-                                      height=self.button_height, bg=c3, font=self.button_font )
+        self.load_button = tk.Button(
+            self.root,
+            text="Load Image",
+            command=self.load_image,
+            width=self.button_width,
+            height=self.button_height,
+            bg=c3,
+            font=self.button_font,
+        )
+        self.annotate_button = tk.Button(
+            self.root,
+            text="Annotate",
+            command=self.annotate_image,
+            width=self.button_width,
+            height=self.button_height,
+            bg=c3,
+            font=self.button_font,
+        )
+        self.quit_button = tk.Button(
+            self.root,
+            text="Quit",
+            command=self.root.quit,
+            width=self.button_width,
+            height=self.button_height,
+            bg=c3,
+            font=self.button_font,
+        )
 
         self.loaded_image = None
         self.annotated_image = None
@@ -75,57 +99,50 @@ class ImageAnnotatorApp:
         self.root.grid_columnconfigure(1, weight=1)
         self.root.grid_columnconfigure(2, weight=1)
 
-        
     def load_image(self):
         self.file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.bmp")])
-        self.image_name=self.file_path.rsplit("/",1)[-1][:-4]
+        self.image_name = self.file_path.rsplit("/", 1)[-1][:-4]
         print("self.image_name", self.image_name)
-        if self.file_path :
+        if self.file_path:
             image = Image.open(self.file_path)
             image.thumbnail((500, 500))  # Resize image to fit in the label
             self.loaded_image = image
-            
             photo = ImageTk.PhotoImage(image)
-
             self.loaded_image_label.config(image=photo)
             self.loaded_image_label.image = photo  # Keep a reference to avoid garbage collection
 
     def annotate_image(self):
         if self.loaded_image is None:
             return
-        
-        SOURCE_IMAGE_PATH = self.file_path
-        
-        # load image
-        image = cv2.imread(SOURCE_IMAGE_PATH)
 
+        SOURCE_IMAGE_PATH = self.file_path
+        image = cv2.imread(SOURCE_IMAGE_PATH)
         annotated_image = self.perform_annotation(image)
         pil_annotated_image = Image.fromarray(cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB))
         pil_annotated_image.thumbnail((500, 500))
         photo = ImageTk.PhotoImage(pil_annotated_image)
-         
         self.annotated_image_label.config(image=photo)
         self.annotated_image_label.image = photo
 
     def perform_annotation(self, image):
-        detections=grounding_dino.predict(image,grounding_dino_model, self.CLASSES)
-        detections=sam.predict(image,sam_model,detections)
+        detections = grounding_dino.predict(image, grounding_dino_model, self.CLASSES)
+        detections = sam.predict(image, sam_model, detections)
+        annotated_image1 = grounding_dino.annotate_image(image, detections, self.CLASSES)
+        cv2.imwrite(os.path.join("data", "annotations", f"{self.image_name}_overlay_bb.png"), annotated_image1)
+        annotated_image = sam.annotate_image(image, detections, self.CLASSES)
+        cv2.imwrite(os.path.join("data", "annotations", f"{self.image_name}_overlay_seg.png"), annotated_image)
 
-        annotated_image1=grounding_dino.annotate_image(image, detections, self.CLASSES)
-        cv2.imwrite(os.path.join("data","annotations",f"{self.image_name}_overlay_bb.png"),annotated_image1)
-        annotated_image=sam.annotate_image(image, detections, self.CLASSES)
-        cv2.imwrite(os.path.join("data","annotations",f"{self.image_name}_overlay_seg.png"),annotated_image)
         ANNOTATIONS_DIRECTORY = os.path.join("data", "annotations")
-        MIN_IMAGE_AREA_PERCENTAGE = 0.002
-        MAX_IMAGE_AREA_PERCENTAGE = 0.80
-        APPROXIMATION_PERCENTAGE = 0.75
-        sv.Dataset(classes=self.CLASSES,images={f"{self.image_name}":image},
-        annotations={f"{self.image_name}":detections}).as_pascal_voc(
-        annotations_directory_path=ANNOTATIONS_DIRECTORY,
-        min_image_area_percentage=MIN_IMAGE_AREA_PERCENTAGE,
-        max_image_area_percentage=MAX_IMAGE_AREA_PERCENTAGE,
-        approximation_percentage=APPROXIMATION_PERCENTAGE)
+        sv.Dataset(
+            classes=self.CLASSES, images={f"{self.image_name}": image}, annotations={f"{self.image_name}": detections}
+        ).as_pascal_voc(
+            annotations_directory_path=ANNOTATIONS_DIRECTORY,
+            min_image_area_percentage=0.002,
+            max_image_area_percentage=0.80,
+            approximation_percentage=0.75,
+        )
         return annotated_image
+
 
 if __name__ == "__main__":
     root = tk.Tk()
--- grounding_dino.py
+++ grounding_dino.py
@@ -11,23 +11,24 @@ import os
 from typing import List
 import supervision as sv
 
-model_path="models"
+model_path = "models"
 
 # wget -q https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth
 GROUNDING_DINO_CONFIG_PATH = os.path.join("models", "GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py")
 GROUNDING_DINO_CHECKPOINT_PATH = os.path.join("weights", "groundingdino_swint_ogc.pth")
 
+
 def load_model():
-    return Model(model_config_path=GROUNDING_DINO_CONFIG_PATH, model_checkpoint_path=GROUNDING_DINO_CHECKPOINT_PATH, device='cpu')
+    return Model(
+        model_config_path=GROUNDING_DINO_CONFIG_PATH, model_checkpoint_path=GROUNDING_DINO_CHECKPOINT_PATH, device="cpu"
+    )
+
 
 def enhance_class_name(class_names: List[str]) -> List[str]:
-    return [
-        f"all {class_name}s"
-        for class_name
-        in class_names
-    ]
+    return [f"all {class_name}s" for class_name in class_names]
 
-def predict(image,grounding_dino_model,CLASSES):
+
+def predict(image, grounding_dino_model, CLASSES):
     BOX_TRESHOLD = 0.35
     TEXT_TRESHOLD = 0.25
     # detect objects
@@ -35,16 +36,14 @@ def predict(image,grounding_dino_model,CLASSES):
         image=image,
         classes=enhance_class_name(class_names=CLASSES),
         box_threshold=BOX_TRESHOLD,
-        text_threshold=TEXT_TRESHOLD
+        text_threshold=TEXT_TRESHOLD,
     )
     return detections
+
 
 def annotate_image(image, detections, CLASSES):
     # annotate image with detections
     box_annotator = sv.BoxAnnotator()
-    labels = [
-        f"{CLASSES[class_id]} {confidence:0.2f}"
-        for _, _, confidence, class_id, _
-        in detections]
+    labels = [f"{CLASSES[class_id]} {confidence:0.2f}" for _, _, confidence, class_id, _ in detections]
     annotated_frame = box_annotator.annotate(scene=image.copy(), detections=detections, labels=labels)
     return annotated_frame
